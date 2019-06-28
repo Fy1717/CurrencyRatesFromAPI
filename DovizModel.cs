01c7b3824cf0e887e76452bd36022c9646b2f241
Furkan Yıldız <frknyldz8489@gmail.com>
2019-06-28 06:03:15 +0300

    First Commit for Only Model and View

---
+++ DovizModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DovizKuru.Models
+{
+    public class DovizModel
+    {
+        public string Doviz { get; set; }
+        public string Satış { get; set; }
+        public string Alış { get; set; }
+
+    }
+}
